061d4a53c86cb308fed0acd613b4e126fa6e7473
Lee Forbes <fillthatinfo@gmail.com>
2024-03-10 15:27:04 -0500

    Initial Commit

---
+++ EZ Password Generator/EZ_Password_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZ_Password_GeneratorApp.swift
+//  EZ Password Generator
+//
+//  Created by Lee Forbes on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EZ_Password_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
